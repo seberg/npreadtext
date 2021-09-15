fbf7f95f09730246f553cc3ff93fbb9a784dd565
Sebastian Berg <sebastian@sipsolutions.net>
2021-09-15 14:42:27 -0500

    ENH: Introduce `from npreadtext import monkeypatch_numpy`
    Also fixes up `loadtxt` to be a bit more clean in its signature
    and documentation.

--- npreadtext/_loadtxt.py
+++ npreadtext/_loadtxt.py
@@ -3,46 +3,50 @@ import numpy as np
 from ._readers import read
 
 
-def _loadtxt(*args, **kwds):
-    delimiter = kwds.pop('delimiter', None)
+def _loadtxt(fname, dtype=float, comments='#', delimiter=None,
+             converters=None, skiprows=0, usecols=None, unpack=False,
+             ndmin=0, encoding='bytes', max_rows=None, **kwargs):
+    """
+    Monkeypatched version of `np.loadtxt`.  Unlike loadtxt it allows some
+    additional keyword arguments, such as `quote='"'`.
+    Please check `npreadtxt.read` for details.
+
+    """
     if delimiter is None:
         delimiter = ''
+    elif isinstance(delimiter, bytes):
+        delimiter.decode("latin1")
 
-    dtype = kwds.pop('dtype', None)
     if dtype is None:
         dtype = np.float64
 
-    ndmin = kwds.pop('ndmin', None)
     if ndmin is None:
         ndmin = 0
     if ndmin not in [0, 1, 2]:
         raise ValueError(f'Illegal value of ndmin keyword: {ndmin}')
 
-    comment = kwds.pop('comments', None)
+    comment = comments
     # Type conversions for Py3 convenience
     if comment is None:
-        comment = '#'
+        comment = ''
     else:
         if isinstance(comment, (str, bytes)):
             comment = [comment]
         comment = [x.decode('latin1') if isinstance(x, bytes) else x for x in comment]
 
-    try:
-        arr = read(*args, delimiter=delimiter, dtype=dtype,
-                   comment=comment, quote='', **kwds)
-    except RuntimeError as exc:
-        raise ValueError(exc.args) from None
+    # Disable quoting unless passed:
+    quote = kwargs.pop('quote', '')
 
-    # For now, ignore 'ndmin' if 'unpack' was given.
-    if 'unpack' not in kwds:
-        if arr.shape == (0, 0):
-            arr.resize((0, 1))
-        if ndmin == 2:
-            if arr.shape == (0, 0):
-                arr = arr.reshape((0, 1))
-        elif ndmin == 1:
-            arr = np.atleast_1d(np.squeeze(arr))
-        else:
-            arr = np.squeeze(arr)
+    arr = read(fname, dtype=dtype, comment=comment, delimiter=delimiter,
+               converters=converters, skiprows=skiprows, usecols=usecols,
+               unpack=unpack, ndmin=ndmin, encoding=encoding,
+               max_rows=max_rows, quote=quote, **kwargs)
 
     return arr
+
+
+try:
+    # Try giving some reasonable docs, but __doc__ could be None.
+    _loadtxt.__doc__ += np.loadtxt.__doc__
+except:
+    pass
